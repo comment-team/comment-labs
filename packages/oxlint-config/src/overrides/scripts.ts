6516b21a86bb933e6621498fe3f40b1808e362ed
Dániel Földi <dani@stefan.hu>
2026-05-01 11:42:24 +0200

    :hammer: Fix scripts override file pattern

--- packages/oxlint-config/src/overrides/scripts.ts
+++ packages/oxlint-config/src/overrides/scripts.ts
@@ -2,7 +2,7 @@ import type { OxlintOverride } from 'oxlint'
 
 
 export const scriptsOverrides: OxlintOverride = {
-  files: [ 'scripts/**/*.?(m)@(j|t)sx' ],
+  files: [ 'scripts/**/*.?(m)@(j|t)s' ],
   rules: {
     'unicorn/no-process-exit': 'warn'
   }
