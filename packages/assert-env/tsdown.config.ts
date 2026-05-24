c9d17626141ff7a3a1413c18e88fa309f170e4aa
Dániel Földi <dani@stefan.hu>
2026-05-24 10:14:57 +0200

    :zap: Also build cjs properly for expo

--- packages/assert-env/tsdown.config.ts
+++ packages/assert-env/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [ 'src/index.ts' ],
-  format: 'esm',
+  format: [ 'esm', 'cjs' ],
   dts: true,
   shims: true,
   sourcemap: true
